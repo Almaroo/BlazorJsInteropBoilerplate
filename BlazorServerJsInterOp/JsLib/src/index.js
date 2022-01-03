a7f5b06b77b2d9ff1df989327fce5ca9eff5ee64
Almaroo <mfxbarszcz@gmail.com>
2022-01-03 21:09:48 +0100

    initial commit

---
+++ BlazorServerJsInterOp/JsLib/src/index.js
@@ -0,0 +1,14 @@
+﻿import { getCurrentTime } from './time_lib';
+import {initializeMap} from "./map_lib";
+
+export function GetCurrentTime() {
+    return getCurrentTime();
+}
+
+export function GetToday() {
+    return getCurrentTime();
+}
+
+export function InitializeMap(options) {
+    return initializeMap(options);
+}
---
+++ BlazorServerJsInterOp/JsLib/src/map_lib.js
@@ -0,0 +1,9 @@
+﻿import L from 'leaflet';
+
+export function initializeMap(options) {
+    const map = L.map('map').setView([49.1830, 20.0851], 20);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
